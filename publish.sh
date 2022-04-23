d72051cfe727f8dd88c7a376ddc0669cb6df8eee
Alexander <electrodeyt@gmail.com>
2022-04-23 22:51:00 +0200

    Change to v1.0.1

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 clickable build --arch arm64
 clickable build --arch amd64
 
-clickable publish --arch arm64
-clickable publish --arch amd64
+# clickable publish --arch arm64
+# clickable publish --arch amd64
